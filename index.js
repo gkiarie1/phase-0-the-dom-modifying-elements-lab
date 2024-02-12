328a1333ced99e398e6a23628e87b0a171c11911
gkiarie1 <gkaris24@gmail.com>
2024-02-12 10:37:43 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.addEventListener("DOMContentLoaded", function(){
+    const mainNode = document.getElementById('main');
+    mainNode.remove();
+
+    const newHeader = document.createElement('h1');
+    newHeader.id ='victory';
+    newHeader.innerHTML ="YOUR-NAME is the champion";
+
+    document.body.appendChild(newHeader);
+});
